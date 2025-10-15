92f16f6474caf84e35cf54b719e035aea842761f
Arjun Avadhani <arjunavadhani22@gmail.com>
2025-10-15 00:15:12 -0400

    feat: Create the Patient class representing an entity that stores all the fields associated with a patient to supplement persistent level validation

---
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -0,0 +1,14 @@
+package com.pm.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// indicates that this is the entry point for the application
+@SpringBootApplication
+public class PatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+}
