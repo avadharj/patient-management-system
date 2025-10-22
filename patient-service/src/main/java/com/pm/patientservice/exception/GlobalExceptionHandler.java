6bb5cd412d4afce119e80f84f9aca2d5a0c57f77
Arjun Avadhani <arjunavadhani22@gmail.com>
2025-10-22 12:58:30 -0400

    feat: Add Global Exception Handling

---
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.pm.patientservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// ControllerAdvice class allows centralized exception handling and makes the controller code a lot cleaner
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    // all the JPA level exceptions like the invalidity of some input etc is caught and placed within ex and this class
+    // allows us to shape the error message to have more detail and show what we want to the user
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex) {
+
+        Map<String,String> errors = new HashMap<>();
+
+        // we are getting all the errors from the ex variable
+        // from that we are getting all the field errors
+        // and,we are adding that info to the errors hashmap
+        // key is the field associated with the error and value is any related error message
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.put(error.getField(),error.getDefaultMessage()));
+
+        // badRequest means that a 400 status code is passed to the calling client along with a json having all errors
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
